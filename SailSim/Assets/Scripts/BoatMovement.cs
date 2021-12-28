23b7497ceb21598847181fc86355d7136768dce9
Valentino Vukelic <tino.vukelic@gmail.com>
2021-12-28 17:45:47 +0100

    Boat movement fixes

--- SailSim/Assets/Scripts/BoatMovement.cs
+++ SailSim/Assets/Scripts/BoatMovement.cs
@@ -10,15 +10,20 @@ public class BoatMovement : MonoBehaviour
     [SerializeField]
     public Wind wind;
 
-    [Tooltip("Za dani kut izmedu jedra i vjetra, daje vrijednost izmedu 0 i 1, sto je postotak moguce maksimalne brzine")]
-    [Curve(0, 0, 180, 1)]
-    [SerializeField]
-    public AnimationCurve trimmingCurve;
-
     [Tooltip("Za dani kut izmedu vjetra i broda, daje vrijednost maksimalne brzine koju brod moze postici")]
     [Curve(0, 0, 180, 2)]
     [SerializeField]
     public AnimationCurve maxSpeedCurve;
+
+    [Tooltip("Za dani kut izmedu vjetra i broda, daje vrijednost najboljeg kuta izmedu jedra i vjetra")]
+    [Curve(0, 0, 180, 100)]
+    [SerializeField]
+    public AnimationCurve bestSailAngleCurve;
+
+    [Tooltip("Za danu gresku u kutu jedra za razliku od najboljeg kuta, daje vrijednost izmedu 0 i 1")]
+    [Curve(0, 0, 180, 1)]
+    [SerializeField]
+    public AnimationCurve trimmingCurve;
 
     [SerializeField]
     public float accelerationFactor;
@@ -31,7 +36,11 @@ public class BoatMovement : MonoBehaviour
 
         var boatWindAngle = Mathf.Abs((transform.rotation.eulerAngles.y - wind.WindDirection + 360 + 180) % 360 - 180);
 
-        var maxSpeed = maxSpeedCurve.Evaluate(boatWindAngle) * trimmingCurve.Evaluate(sailWindAngle) * wind.WindSpeed;
+        var bestSailAngle = bestSailAngleCurve.Evaluate(boatWindAngle);
+
+        var angleDelta = Mathf.Abs(bestSailAngle - sailWindAngle);
+
+        var maxSpeed = maxSpeedCurve.Evaluate(boatWindAngle) * trimmingCurve.Evaluate(angleDelta) * wind.WindSpeed;
 
         var speedDelta = maxSpeed - CurrentSpeed;
 
