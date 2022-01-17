53e720a70b991be3b2b37072b126abf2f8d1e90f
lb52804 <lb52804@fer.hr>
2022-01-18 00:49:31 +0100

    Fix model oceana osjetljiv na promjene brzine vjetra

--- SailSim/Assets/Scripts/Wind.cs
+++ SailSim/Assets/Scripts/Wind.cs
@@ -1,3 +1,4 @@
+using Crest;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,15 @@ public class Wind : MonoBehaviour
 {
     [SerializeField]
     public ScenarioSettings scenarioSettings;
+
+    [SerializeField]
+    public ShapeGerstnerBatched shapeGerstnerBatched;
+
+    [SerializeField]
+    public float minWindSpeed;
+
+    [SerializeField]
+    public float maxWindSpeed;
 
     public float WindSpeed { get; private set; }
     public float WindDirection { get; private set; }
@@ -19,6 +29,16 @@ public class Wind : MonoBehaviour
     {
         WindSpeed = scenarioSettings.windSpeed;
         WindDirection = scenarioSettings.windDirection % 360;
+
+        //shapeGerstnerBatched._windDirectionAngle = WindDirection;
+
+        if (maxWindSpeed < WindSpeed)
+            maxWindSpeed = WindSpeed;
+        else if (minWindSpeed > WindSpeed)
+            minWindSpeed = WindSpeed;
+
+        shapeGerstnerBatched._weight =  (WindSpeed - minWindSpeed)
+                                        / (maxWindSpeed - minWindSpeed);
     }
 
     public void Update()
